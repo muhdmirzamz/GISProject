af673e7bb6aa4d51116a20fe2e8915ee3b2adf3b
Grift <fjh828@gmail.com>
2016-06-22 15:04:23 +0800

    Small change

--- GISProject/SignupViewController.swift
+++ GISProject/SignupViewController.swift
@@ -80,7 +80,6 @@ class SignupViewController: UIViewController {
                                     let level : NSNumber = 1
                                     let monst : NSNumber = 0
                                     let cards : NSNumber = 0
-                                    let 
                                     
                                     let key = self.ref.child("Account/\(uid)").key
                                     let Account = ["Name": self.UsernameLabel.text!, "Base damage" : base, "Level" : level, "Monsters killed": monst, "Cards" : cards]
