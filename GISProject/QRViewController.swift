dd30a29b7d93b315f4421a39f252c0b31c9c39eb
Grift <fjh828@gmail.com>
2016-06-22 14:24:35 +0800

    Added icons for QR & Profile

---
+++ GISProject/QRViewController.swift
@@ -0,0 +1,25 @@
+//
+//  QRViewController.swift
+//  GISProject
+//
+//  Created by Jun Hui Foong on 22/6/16.
+//  Copyright © 2016 NYP. All rights reserved.
+//
+
+import UIKit
+
+class QRViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+
+}
