1365d1cad5b8b99ba1f1a332d09645360917ceff
Grift <fjh828@gmail.com>
2016-06-22 11:32:46 +0800

    logout

--- GISProject/ProfileViewController.swift
+++ GISProject/ProfileViewController.swift
@@ -69,16 +69,20 @@ class ProfileViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func dismiss(){
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
-    */
+    
+    @IBAction func logout() {
+        
+        do {
+            try! FIRAuth.auth()!.signOut()
+            self.dismiss()
+        } catch {
+            
+        }
+
+    }
 
 }
