2c3a168adc681e14393df417ebdec02fd81396e8
towwell <towwell@gmail.com>
2016-07-11 10:25:17 +0800

    deleted php file

--- GISProject/Battle.swift
+++ GISProject/Battle.swift
@@ -13,6 +13,8 @@ class Battle: NSObject {
     var monsterHealth: Float?
     var amountOfCards: NSNumber?
     var amountOfCardsToUse: NSNumber?
+    var cardsArr: NSMutableArray?
+    var uidArr: NSMutableArray?
     
     var expectedMonsterHealth: Float?
     var timer: NSTimer?
@@ -20,6 +22,11 @@ class Battle: NSObject {
     override init() {
         self.monsterHealth = 1
         self.amountOfCardsToUse = 0
+        
+        self.cardsArr = NSMutableArray()
+        self.cardsArr?.addObject(0)
+        
+        self.uidArr = NSMutableArray()
     }
     
     func getMonsterHealth() -> Int {
@@ -28,5 +35,24 @@ class Battle: NSObject {
     
     func getAmountOfCardsToUse() -> Int {
         return (self.amountOfCardsToUse?.integerValue)!
+    }
+    
+    func checkUserCardSwitch() {
+        let scheduledLocalNotifCount = UIApplication.sharedApplication().scheduledLocalNotifications!.count
+        print(scheduledLocalNotifCount)
+        
+        // user has used card
+        if scheduledLocalNotifCount > 0 {
+            self.userCardSwitch.on = false
+            self.userCardSwitch.enabled = false
+        } else {
+            self.userCardSwitch.on = true
+            
+            if self.amountOfCardsToUse! == 0 || self.textfield.text == "0" {
+                self.userCardSwitch.enabled = false
+            } else {
+                self.userCardSwitch.enabled = true
+            }
+        }
     }
 }
