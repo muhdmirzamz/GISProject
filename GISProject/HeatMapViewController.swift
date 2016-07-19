80169330d2e31cde542b0fbd8385002dd38ecf9e
Grift <fjh828@gmail.com>
2016-07-19 16:51:32 +0800

    PageMenu working

---
+++ GISProject/HeatMapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HeatMapViewController.swift
+//  GISProject
+//
+//  Created by Jun Hui Foong on 19/7/16.
+//  Copyright © 2016 NYP. All rights reserved.
+//
+
+import UIKit
+
+class HeatMapViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let localfilePath = NSBundle.mainBundle().URLForResource("heatmap", withExtension: "html");
+        let myRequest = NSURLRequest(URL: localfilePath!);
+        webView.loadRequest(myRequest);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
