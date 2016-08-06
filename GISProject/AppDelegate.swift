7bbaad32d85284a48a97b2fca3dc3898c072d522
XINGYU <XINGYU@xingyus-macbook-pro.local>
2016-08-06 12:35:48 +0800

    able to update online and offline while app close or become foreground

--- GISProject/AppDelegate.swift
+++ GISProject/AppDelegate.swift
@@ -182,7 +182,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate,CLLocationManagerDelegate 
 	func applicationWillResignActive(application: UIApplication) {
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 		// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-        
+        if KEY_UID != "" {
+             onlineUserRef.child(KEY_UID).updateChildValues(["KEY_ISONLINE":false])
+        }
 	}
 
 	func applicationDidEnterBackground(application: UIApplication) {
@@ -192,7 +194,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate,CLLocationManagerDelegate 
 
 	func applicationWillEnterForeground(application: UIApplication) {
 		// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-        
+        if KEY_UID != "" {
+            onlineUserRef.child(KEY_UID).updateChildValues(["KEY_ISONLINE":true])
+        }
 	}
 
 	func applicationDidBecomeActive(application: UIApplication) {
