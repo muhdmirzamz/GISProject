11291b68496944bcb3617956724da94d763f62e4
Grift <fjh828@gmail.com>
2016-08-07 12:39:18 +0800

    [SWIFT] Cleaned Design, Code & Assets

--- GISProject/IntroViewController.swift
+++ GISProject/IntroViewController.swift
@@ -134,15 +134,7 @@ class IntroViewController: UIViewController, ProfileProtocol {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
     }
-    */
-
 }
